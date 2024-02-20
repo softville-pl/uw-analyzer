1aafee1503f67af45d7d45343d398a88102e96d3
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-02-20 16:22:48 +0100

    Playing with iac using CDK for TF

---
+++ deployments/Softville.Prospecting.Infra/AzureInfrastrucutre.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Constructs;
+using HashiCorp.Cdktf.Providers.Azurerm.CosmosdbAccount;
+using HashiCorp.Cdktf.Providers.Azurerm.ResourceGroup;
+
+namespace Softville.Prospecting.Infra;
+
+public class AzureInfrastructure : Construct
+{
+    public AzureInfrastructure(Construct scope, string id, string resourceGroupName, string cosmosDbName,
+        string environment) : base(scope, id)
+    {
+        // Define the Resource Group
+        Dictionary<string, string> tags = new() {{"env", environment}, {"app", "prospecting-app"}};
+
+        ResourceGroup resourceGroup = new(this, "ResourceGroup",
+            new ResourceGroupConfig {Name = resourceGroupName, Location = "polandcentral", Tags = tags});
+
+        // Define the CosmosDB Account
+        new CosmosdbAccount(this, "CosmosDbAccount",
+            new CosmosdbAccountConfig
+            {
+                Name = cosmosDbName,
+                ResourceGroupName = resourceGroup.Name,
+                Location = resourceGroup.Location,
+                OfferType = "Standard",
+                Kind = "GlobalDocumentDB",
+                ConsistencyPolicy = new CosmosdbAccountConsistencyPolicy {ConsistencyLevel = "Session"},
+                GeoLocation =
+                    new CosmosdbAccountGeoLocation[] {new() {Location = resourceGroup.Location, FailoverPriority = 0}},
+                EnableMultipleWriteLocations = false,
+                EnableFreeTier = true,
+                Capacity = new CosmosdbAccountCapacity {TotalThroughputLimit = 1000},
+                Tags = tags
+            });
+    }
+}
---
+++ deployments/Softville.Prospecting.Infra/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using HashiCorp.Cdktf;
+using Softville.Prospecting.Infra;
+
+App app = new();
+new AzureInfrastructure(app, "MyAzureInfrastructure",
+    "rg-prspct-test-plc-001",
+    "cosmos-prspct-test-plc-001",
+    "test");
+app.Synth();
