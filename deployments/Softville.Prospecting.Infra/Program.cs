e37d3d9c4a3d70dac7e77f4969c3d739d7aa48c1
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-02-22 07:49:43 +0100

    Azure storage as a remote state backend

--- deployments/Softville.Prospecting.Infra/Program.cs
+++ deployments/Softville.Prospecting.Infra/Program.cs
@@ -4,6 +4,18 @@ using HashiCorp.Cdktf;
 using Softville.Prospecting.Infra;
 
 App app = new();
+
+// Define the Remote State Configuration
+new AzurermBackend(app,
+    new AzurermBackendConfig
+    {
+        ResourceGroupName = "rg-prspct_infra-test-plc-001",
+        StorageAccountName = "stprspctinfratestplc",
+        ContainerName = "terraformstate",
+        Key = "prspct-test-plc.terraform.tfstate"
+    });
+
+// Register the Stack to your App
 new AzureInfrastructure(app, "MyAzureInfrastructure",
     "rg-prspct-test-plc-001",
     "cosmos-prspct-test-plc-001",
--- deployments/create-storage-terraform-state.ps1
+++ deployments/create-storage-terraform-state.ps1
@@ -3,13 +3,13 @@ $rg_name = 'rg-prspct_infra-test-plc-001'
 $app_tag = 'app=prospecting-app'
 $env_tag = 'env=test'
 $scenario_tag = 'scenario=terraform_state'
-$st_name = 'st-prspct_infra-test-plc'
+$st_name = 'stprspctinfratestplc'
 
 # Create a resource group if you don't already have one
-# az group create `
-#     --name $rg_name `
-#     --location $location `
-#     --tags $app_tag $env_tag $scenario_tag
+az group create `
+    --name $rg_name `
+    --location $location `
+    --tags $app_tag $env_tag $scenario_tag
 
 # Create the storage account
 az storage account create `
