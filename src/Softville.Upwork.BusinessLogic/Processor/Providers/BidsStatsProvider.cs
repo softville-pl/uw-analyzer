699ecb48a444ef707bd3536b4972c6de1098e207
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-02-26 21:38:53 +0100

    Bids stats WIP

---
+++ src/Softville.Upwork.BusinessLogic/Processor/Providers/BidsStatsProvider.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Softville.Upwork.BusinessLogic.Processor.Providers;
+
+internal class BidsStatsProvider : IBidsStatsProvider
+{
+    public Task NAME(TYPE2 NAME2)
+    {
+    }
+
+}
