fef633ca59505a46ff144f995c0b09da1fd21ebe
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-03-08 07:08:05 +0100

    AzureFunc for offers processor v1

---
+++ deployments/Softville.Prospecting.Infra/Resources/StorageCreator.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using HashiCorp.Cdktf.Providers.Azurerm.ResourceGroup;
+using HashiCorp.Cdktf.Providers.Azurerm.StorageAccount;
+
+namespace Softville.Prospecting.Infra.Resources;
+
+internal class StorageCreator
+{
+    internal static StorageAccount CreateStorageAccount(ResourceCreatorContext context, ResourceGroup resourceGroup)
+    {
+        StorageAccount st = new(context.Scope, "StorageAccount",
+            new StorageAccountConfig
+            {
+                Name = $"st{context.Infrastructure.GetStorageAccountNameNamePostfix()}",
+                ResourceGroupName = resourceGroup.Name,
+                Location = resourceGroup.Location,
+                AccountKind = "StorageV2",
+                AccountTier = "Standard",
+                AccountReplicationType = "LRS",
+                MinTlsVersion = "TLS1_2",
+                EnableHttpsTrafficOnly = true,
+                PublicNetworkAccessEnabled = false,
+                SharedAccessKeyEnabled = true,
+                NetworkRules =
+                    new StorageAccountNetworkRules {DefaultAction = "Allow", Bypass = ["Logging", "AzureServices"]},
+                BlobProperties = new StorageAccountBlobProperties
+                {
+                    DeleteRetentionPolicy = new StorageAccountBlobPropertiesDeleteRetentionPolicy {Days = 7,},
+                    ContainerDeleteRetentionPolicy =
+                        new StorageAccountBlobPropertiesContainerDeleteRetentionPolicy {Days = 7},
+                },
+                AccessTier = "Hot"
+            });
+        return st;
+    }
+}
