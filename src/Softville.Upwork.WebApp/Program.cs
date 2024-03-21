36fbc8b6a0b1342581f170b0f5db666acceee4e3
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-03-21 12:50:19 +0100

    WIP WebApp fetches offer from API

--- src/Softville.Upwork.WebApp/Program.cs
+++ src/Softville.Upwork.WebApp/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 using Softville.Upwork.WebApp;
+using Softville.Upwork.WebApp.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.Configure<WebAppConfig>(builder.Configuration.GetSection(WebAppConfig.ConfigSectionName));
+builder.Services.Configure<BackendConfig>(builder.Configuration.GetSection(WebAppConfig.ConfigSectionName).GetSection(BackendConfig.ConfigSectionName));
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(sp.GetRequiredService<IOptions<BackendConfig>>().Value.BaseUrl)
+});
 
 await builder.Build().RunAsync();
